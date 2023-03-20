cd91431f2cbe8ec0dbf7d93520ba589cd98b00ff
Tristan Shawn den Ouden <trisdo687@gmail.com>
2023-03-20 03:48:36 +0100

    moved

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import Prompt from "../PromptAI";
+import Prompt from "../services/PromptAI";
 import styles from "./ChatBot.module.css";
 
 interface ChatMessage {
